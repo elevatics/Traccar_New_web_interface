b6b044ecf81e1c8db207171079500debe0ad1967
Yash Bele <krupabele47@gmail.com>
2026-04-15 19:06:40 +0530

    removed favicon

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+(() => {
+  const blank = "data:,";
+  document
+    .querySelectorAll(
+      'link[rel="icon"], link[rel="shortcut icon"], link[rel="apple-touch-icon"], link[rel="apple-touch-icon-precomposed"]'
+    )
+    .forEach((el) => el.remove());
+  const link = document.createElement("link");
+  link.rel = "icon";
+  link.href = blank;
+  document.head.appendChild(link);
+})();
+
 createRoot(document.getElementById("root")!).render(<App />);
